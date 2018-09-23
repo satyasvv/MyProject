a8871b7ff19908e8e2b9f71abe27b98b620f93ff
satyasvv <42281860+satyasvv@users.noreply.github.com>
2018-09-23 15:51:07 -0500

    commit-test

---
+++ myscala-project/src/myclass.scala
@@ -0,0 +1,17 @@
+
+class myclass {
+  def test(): Unit = {
+       println("This is a test scla program")
+  }
+}
+
+
+object testmyclass {
+  
+    def main(args: Array[String]): Unit ={
+     
+      val m = new myclass()
+      m.test()
+      
+    }
+     }
